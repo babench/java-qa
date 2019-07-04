1af1809f35ce568d5f856b176f76389e4ba3057c
Alexander Zaikin <allexzaikin@gmail.com>
2019-07-04 21:13:27 +0300

    refactoring. Common in Parent POM

--- homework09/ejb-service/src/main/java/ru/otus/zaikin/HelloSessionBean.java
+++ homework09/ejb-service/src/main/java/ru/otus/zaikin/HelloSessionBean.java
@@ -2,14 +2,16 @@ package ru.otus.zaikin;
 
 import java.util.Date;
 
-/*import javax.ejb.Local;
+/*
+import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import java.util.Date;
 
 @Stateless(name = "HelloSessionBean", mappedName = "ejb/HelloSessionBean")
 @Local( HelloSessionBeanLocal.class  )
-@Remote( HelloSessionBeanRemote.class )*/
+@Remote( HelloSessionBeanRemote.class )
+*/
 public class HelloSessionBean implements HelloSessionBeanLocal, HelloSessionBeanRemote  {
     public Date getTime() {
         return new Date();
