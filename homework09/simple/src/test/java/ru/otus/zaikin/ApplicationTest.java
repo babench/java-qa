ce6f418aed3fc48a89bac0b8b26008e7032e8a6a
Alexander Zaikin <allexzaikin@gmail.com>
2019-07-04 21:07:27 +0300

    refactoring. Common in Parent POM

---
+++ homework09/simple/src/test/java/ru/otus/zaikin/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.otus.zaikin;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ApplicationTest {
+
+    @Test
+    public void shouldWork() {
+        assertTrue(true);
+    }
+}
