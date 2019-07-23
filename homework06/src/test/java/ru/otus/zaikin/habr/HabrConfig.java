ddd5864b73303ede604ab4eccc3e566bef63da47
Alexander Zaikin <allexzaikin@gmail.com>
2019-07-23 22:27:14 +0300

    habr online)

---
+++ homework06/src/test/java/ru/otus/zaikin/habr/HabrConfig.java
@@ -0,0 +1,7 @@
+package ru.otus.zaikin.habr;
+
+public class HabrConfig {
+    public static final String URL = "https://habr.com/ru/";
+    public static final String URL_HUBS = "https://habr.com/ru/hubs/";
+
+}
---
+++ homework06/src/test/java/ru/otus/zaikin/habr/HabrHubPageTest.java
@@ -0,0 +1,38 @@
+package ru.otus.zaikin.habr;
+
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.otus.zaikin.framework.DriverBase;
+
+import java.net.MalformedURLException;
+import java.security.Key;
+
+@Log4j2
+public class HabrHubPageTest extends DriverBase {
+
+    private WebDriver driver;
+
+
+    @BeforeMethod
+    public void setup() throws MalformedURLException {
+        driver = getDriver();
+        driver.get(HabrConfig.URL_HUBS);
+    }
+
+    @Test
+    public void shouldOpenItSystems() {
+        By locator = By.cssSelector("#hubs .list-snippet__title-link");
+        String hubName = "Тестирование IT-систем";
+        driver.findElement(By.cssSelector("#hubs_suggest")).sendKeys(hubName);
+        driver.findElement(By.cssSelector("#hubs_suggest")).sendKeys(Keys.ENTER);
+        new WebDriverWait(driver, 10).until(ExpectedConditions.textToBe(locator, hubName));
+        log.debug("here");
+    }
+
+}
